fae5d669c12b4c9da90b1bd66b043a0c5414c89b
Yahor Kuzaukou <egkuzoff@gmail.com>
2022-05-08 19:30:56 +0300

    Add first and start second task

---
+++ task_1.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def get_inv(A, A_inv, x):
+    i = len(x) - 1
+    # Замена i-го столбца на вектор-столбец x
+    for j in range(len(x)):
+        A[j][i] = x[j]
+    # Шаг 1. Находим l = A_inv * x и проверяем, существует ли обратная матрица.
+    l = np.matmul(A_inv, x)
+    if l[i] == 0:
+        return None
+
+    # Шаг 2. В копии вектора l заменим i-й элемент на -1
+    l_copy = np.copy(l)
+    l_copy[i] = -1
+
+    # Шаг 3. Находим l_dashed
+    l_dashed = (-1 / l[i]) * l_copy
+
+    # Шаг 4. Матрица Q формируется из единичной матрицы порядка n заменой i-го столбца на l_dashed
+    Q = np.eye(3)
+    for j in range(len(l_dashed)):
+        Q[j][i] = l_dashed[j]
+
+    #Шаг 5. Находим обратную матрицу путем умножения Q на A_inv 
+    res = np.zeros([3,3])
+    for j in range(len(x)):
+        for k in range(len(x)):
+            first = Q[j][j] * A_inv[j][k]
+            second = Q[j][i] * A_inv[i][k]
+            res[j][k] = first + second if j != k else first
+    return res
+
+
+A = np.array([[1, -1, 0], [0, 1, 0], [0, 0, 1]])
+A_inv = np.array([[1, 1, 0], [0, 1, 0], [0, 0, 1]])
+x = np.array([1, 0, 1])
---
+++ task_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from task_1 import get_inv
+
+
+c = np.array([1, 1, 0, 0, 0])
+x = np.array([0, 0, 1, 3, 2])
+B = np.array([3, 4, 5])
+A = np.array([
+    [-1, 1, 1, 0, 0],
+    [1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 1]
+])
+
